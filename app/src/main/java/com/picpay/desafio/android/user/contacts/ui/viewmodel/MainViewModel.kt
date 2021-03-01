4dd792bf1de1dbd1e3de10b34bfe3e9f400a873a
Vinicius Azevedo <vinicius.azevedo@bancointer.com.br>
2021-03-01 20:39:52 -0300

    ajuste final viewmodel

--- app/src/main/java/com/picpay/desafio/android/user/contacts/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/user/contacts/ui/viewmodel/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.picpay.desafio.android.user.contacts.ui.viewmodel
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -18,19 +19,20 @@ class MainViewModel(
     private val coroutineDispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : ViewModel() {
 
-    val fetchContactsLiveData = MutableLiveData<ViewState<List<UserViewData>>>()
+    private val _fetchContactsLiveData = MutableLiveData<ViewState<List<UserViewData>>>()
+    val fetchContactsLiveData: LiveData<ViewState<List<UserViewData>>> = _fetchContactsLiveData
 
     fun fetch() {
         viewModelScope.launch(Dispatchers.Main) {
-            fetchContactsLiveData.value = ViewState.Loading
+            _fetchContactsLiveData.value = ViewState.Loading
             val result = withContext(coroutineDispatcher) {
                 fetchContactsUseCase.execute()
             }
-            fetchContactsLiveData.value = when (result) {
+            _fetchContactsLiveData.value = when (result) {
                 is State.Success -> ViewState.Success(result.data.toUserViewData())
                 is State.Failure -> ViewState.Failure(result.error)
             }
-            fetchContactsLiveData.value = ViewState.Done
+            _fetchContactsLiveData.value = ViewState.Done
         }
     }
 }
